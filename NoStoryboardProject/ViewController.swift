fe4a36d39e1fdd8abbe45cf1f8a2bbfb2436b37a
ليزبيث هالا <llulah@live.com>
2022-11-09 16:50:05 -0300

    Added the final project

---
+++ NoStoryboardProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryboardProject
+//
+//  Created by ليزبيث هالا on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
